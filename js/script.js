e83f87e460b85144228f4d133a598ac15f41f335
nevardamachubby <nevardamachubby28@gmail.com>
2024-07-03 04:55:52 -0700

    update website

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-// Ini Javascript
+// Message us content
+function submitForm() {
+    const name = document.getElementById('name').value;
+    const dob = document.getElementById('dob').value;
+    const genderElement = document.querySelector('input[name="gender"]:checked');
+    const gender = genderElement ? genderElement.value : '';
+    const message = document.getElementById('message').value;
+
+    document.getElementById('currentTime').innerText = new Date().toLocaleString();
+    document.getElementById('outputName').innerText = name;
+    document.getElementById('outputDob').innerText = dob;
+    document.getElementById('outputGender').innerText = gender;
+    document.getElementById('outputMessage').innerText = message;
+
+    console.log("Form submitted successfully!");
+    console.log("Name: " + name);
+    console.log("DOB: " + dob);
+    console.log("Gender: " + gender);
+    console.log("Message: " + message);
+}
+
+
+
+
+
